c022c6b30ba46af44a53b3e6135aef92f0d40881
Kos <koshuang@gmail.com>
2021-07-17 13:19:08 +0800

    refactor(component): [App] extract a new componnet MoodReaction

--- src/modules/app/App.jsx
+++ src/modules/app/App.jsx
@@ -1,30 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
 
-export function App() {
-  const message = 'How are you?';
-
+function MoodReaction() {
   function respondToHappy() {
     alert('Awesome!');
   }
 
   function respondToBlue() {
-    alert('It\'s okay!');
+    alert("It's okay!");
   }
+
+  return (
+    <div>
+      <p>
+        <button onClick={respondToHappy}>I am happy.</button>
+      </p>
+      <p>
+        <button onClick={respondToBlue}>I am blue.</button>
+      </p>
+    </div>
+  );
+}
+
+export function App() {
+  const message = 'How are you?';
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hi, {message}
-        </p>
-        <p>
-          <button onClick={respondToHappy}>I am happy.</button>
-        </p>
-        <p>
-          <button onClick={respondToBlue}>I am blue.</button>
-        </p>
+        <p>Hi, {message}</p>
+        <MoodReaction />
       </header>
     </div>
   );
