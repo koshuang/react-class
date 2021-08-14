498c4fd96906932450874e71342e2fc060cc7a91
Kos Huang <koshuang@gmail.com>
2021-08-14 16:57:15 +0800

    feat(app): integrate react-router
    - `/`: for home page
    - `/todos`: for todo list page

--- src/modules/app/App.jsx
+++ src/modules/app/App.jsx
@@ -1,19 +1,39 @@
-import { Food, MoodReaction } from '../mood';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import { Home } from '../home';
+import { TodoList } from '../todo-list';
 import './App.css';
 import logo from './logo.svg';
 
 export function App() {
-  const message = 'How are you?';
-  const userName = 'Apple';
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hi, {message}</p>
-        <MoodReaction name={userName} />
-        <Food />
-      </header>
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+      />
+      <Router>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/todos">Todos</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path="/todos">
+              <TodoList />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </header>
+      </Router>
     </div>
   );
 }
