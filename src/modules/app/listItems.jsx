e999a0c35f00748c4fd33ffbb73caecf2e8df924
Kos Huang <koshuang@gmail.com>
2021-08-21 17:06:27 +0800

    feat(app/component): [MenuItems] new

---
+++ src/modules/app/listItems.jsx
@@ -0,0 +1,24 @@
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import DashboardIcon from '@material-ui/icons/Dashboard';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+
+export const mainListItems = (
+  <div>
+    <ListItem button component={RouterLink} to="/">
+      <ListItemIcon>
+        <DashboardIcon />
+      </ListItemIcon>
+      <ListItemText primary="Home" />
+    </ListItem>
+    <ListItem button component={RouterLink} to="/todos">
+      <ListItemIcon>
+        <ShoppingCartIcon />
+      </ListItemIcon>
+      <ListItemText primary="Todos" />
+    </ListItem>
+  </div>
+);
